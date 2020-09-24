0ac28145273a58d4070cdb8d7404155acc3fab1b
Bogdan Radoi <radoi.office@gmail.com>
2020-09-24 15:15:20 +0200

    REDIRIGE un User qui essaie d'aller sur une page admin Vers route : home

---
+++ src/Security/AccessDeniedHandler.php
@@ -0,0 +1,27 @@
+<?php
+namespace App\Security;
+
+
+use Symfony\Component\HttpFoundation\RedirectResponse;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
+use Symfony\Component\Security\Core\Exception\AccessDeniedException;
+use Symfony\Component\Security\Http\Authorization\AccessDeniedHandlerInterface;
+
+class AccessDeniedHandler implements AccessDeniedHandlerInterface
+{
+    /**
+     * @var UrlGeneratorInterface
+     */
+    private $urlGenerator;
+
+    public function __construct(UrlGeneratorInterface $urlGenerator)
+    {
+        $this->urlGenerator = $urlGenerator;
+    }
+
+    public function handle(Request $request, AccessDeniedException $accessDeniedException)
+    {
+        return new RedirectResponse($this->urlGenerator->generate('home'));
+    }
+}
