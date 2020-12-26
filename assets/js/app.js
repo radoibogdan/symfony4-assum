44976f2aeb06e9770c23e064edb906e5681c4d9f
YBogdan RADOI <radoi.office@gmail.com>
2020-12-26 14:50:40 +0100

    ADD SIMPLE MARKDOWN EDITOR

--- assets/js/app.js
+++ assets/js/app.js
@@ -11,4 +11,25 @@ import '../css/app.scss';
 // Need jQuery? Install it with "yarn add jquery", then uncomment to import it.
 // import $ from 'jquery';
 
+// IMPORTER LE JS DU EDITEUR DE MARKDOWN
+import SimpleMDE from "simplemde";
 
+// CHARGER LE CSS
+require('simplemde/dist/simplemde.min.css');
+
+// CONFIGURER L'EDITEUR SIMPLE MDE
+document.addEventListener('DOMContentLoaded', function () {
+    let textarea = document.getElementById('slide_content');
+    let editor_name = 'slide_' + textarea.dataset.slideId;
+
+    var simplemde = new SimpleMDE({
+        autosave: {
+            enabled: true,
+            uniqueId: editor_name
+        },
+        element: textarea,
+        forceSync: true,
+        spellChecker: false,
+        tabSize: 4
+    });
+});
